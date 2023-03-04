e97b4fc26e3518e548813f0f05efa615e9d6f9da
Aoi <naztya2001@bk.ru>
2023-03-04 10:23:42 +0300

    Task17

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Quater(int xC, int yC){
+    if (xC>0 && yC>0) return 1;
+    if (xC<0 && yC>0) return 2;
+    if (xC<0 && yC<0) return 3;
+    if (xC>0 && yC<0) return 4;
+    return 0;
+}
+
+Console.WriteLine("Input the coordinates");
+Console.Write("X");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int quater = Quater(x,y);
+string result = quater>0 
+                ? $"The dot is in {quater}th quater" 
+                : "Choose differend numbers";
+
+Console.WriteLine(result);
+

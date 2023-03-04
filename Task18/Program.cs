ded54820a68dbf8de482b994664053e1c8d3a922
Aoi <naztya2001@bk.ru>
2023-03-04 10:40:32 +0300

    task18

---
+++ Task18/Program.cs
@@ -0,0 +1,17 @@
+﻿string Quater(int number){
+    if (number == 1) return "x>0 && y>0";
+    if (number == 2) return "x<0 && y>0";
+    if (number == 3) return "x<0 && y<0";
+    if (number == 4) return "x>0 && y<0";
+    return "Choose differend number";
+}
+
+Console.WriteLine("Input the quater");
+int x = Convert.ToInt32(Console.ReadLine());
+
+string quater = Quater(x);
+string result = x>0 && x<5 
+                ? $"The dots can be in {quater}" 
+                : "Choose differend number";
+
+Console.WriteLine(result);

bff60529bb1b522523611dea46d18ce1f88d7c4d
Aoi <naztya2001@bk.ru>
2023-03-04 11:40:25 +0300

    task22

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Input number from N");
+int number = Convert.ToInt32(Console.ReadLine());
+
+SquaresTable(number);
+
+void SquaresTable(int num)
+{
+    int i = 1;
+    while(i<=num)
+    {
+        Console.WriteLine($"Number: {i,3}, It squared: {i*i,4}");
+        i++;
+    }
+}
+

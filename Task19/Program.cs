09a26f6c32cd28f541528edc88cff919acd69b84
Aoi <naztya2001@bk.ru>
2023-03-07 12:32:43 +0300

    task19

---
+++ Task19/Program.cs
@@ -0,0 +1,27 @@
+﻿// 5-digit number is polindrome
+
+bool IsPolindrome(int num)
+{
+        if (num/10000 == num%10)
+        {
+            num=(num/10000-num%10)/10;
+            if (num/100 == num%10)
+            {
+                return true;
+            }
+        }
+    return false;
+}
+
+void DoPolindrome(bool pol)
+{
+    if (pol) Console.WriteLine("Yes"); else Console.WriteLine("No");
+}
+
+Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number/100000 == 0 && number/10000 != 0) DoPolindrome(IsPolindrome(number)); 
+else Console.WriteLine("Choose a differend number");
+
+

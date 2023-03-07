86a9db1efee355d438c669288cc746c7deaaa8a4
Aoi <naztya2001@bk.ru>
2023-03-07 12:43:40 +0300

    task23

---
+++ Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Input number from N");
+int number = Convert.ToInt32(Console.ReadLine());
+
+SquaresTable(number);
+
+void SquaresTable(int num)
+{
+    int i = 1;
+    while(i<=num)
+    {
+        Console.WriteLine($"{i,3} | {i*i*i,5}");
+        i++;
+    }
+}
